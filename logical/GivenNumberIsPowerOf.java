7217f6d1a06e7175721d4a42a4208c2a044a9910
Rahul <rahulkumar.it@outlook.com>
2025-01-20 06:24:22 +0530

    added in main method example for power of

--- logical/GivenNumberIsPowerOf.java
+++ logical/GivenNumberIsPowerOf.java
@@ -2,7 +2,7 @@ package logical;
 
 public class GivenNumberIsPowerOf {
 
-    //2,4
+
     public static boolean isPower(int powerOf, int givenNumber) {
         if (givenNumber == 0)
             return false;
@@ -18,7 +18,7 @@ public class GivenNumberIsPowerOf {
 
 
     public static void main(String[] args) {
-        int number=32;
-        System.out.println(isPower(4,number));
+        //2*2*2*2=16
+        System.out.println(isPower(4,16));
     }
 }
